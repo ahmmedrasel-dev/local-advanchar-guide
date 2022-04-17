1b16af4132f15f0376f3f1bf359740db7b88a85b
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-04-17 05:00:16 +0300

    firebase install and deploy hosting

--- src/Pages/LoginRegister/Login/Login.js
+++ src/Pages/LoginRegister/Login/Login.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
+  const navigateRegister = () => {
+    navigate('/register')
+  }
+
   return (
-    <div>
-      <h1>This is Login Component</h1>
-    </div>
-  );
-};
+    <section className='my-5'>
+      <Container>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form onClick={handleSubmit} className='border p-5 rounded-3'>
+              <div className='text-center'>
+                <h3>Please Login</h3>
+                <p>Login with Your Email & Password.</p>
+              </div>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" required />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" required />
+              </Form.Group>
+              <Button variant="primary" type="submit" className='w-100'>
+                Submit
+              </Button>
+
+              <p className='text-center mt-3'>Don't have an account? <span style={{ 'cursor': 'pointer', 'color': 'red' }} onClick={navigateRegister}>Register</span></p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
 
 export default Login;
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCvyy_EKcQsdoOIZtSOqLNIVoeZf0Ghf8",
+  authDomain: "local-advancer-guide.firebaseapp.com",
+  projectId: "local-advancer-guide",
+  storageBucket: "local-advancer-guide.appspot.com",
+  messagingSenderId: "232330145620",
+  appId: "1:232330145620:web:ac8a9a34a820208481a2c1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
